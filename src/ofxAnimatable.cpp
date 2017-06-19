373376ac771fe43d8ad0bdf1dff684ffcefa96dc
Oriol Ferrer Mesia <oriol@uri.cat>
2017-06-19 12:29:49 -0400

    fix for GLM stuff while hopefully staying backward compatibility

--- src/ofxAnimatable.cpp
+++ src/ofxAnimatable.cpp
@@ -696,6 +696,8 @@ void ofxAnimatable::drawCurve(int x, int y, int size, bool bg, ofColor c ){
 		glPointSize(4);
 		pts.setMode(OF_PRIMITIVE_POINTS);
 		int blink = (ofGetFrameNum()%6 > 2) ? 255 : 0;
+		auto pt0 = pts.getVertices()[0];
+		auto pt1 = pts.getVertices()[1];
 		switch (*curveStylePtr_) {
 			case CUBIC_BEZIER2_PARAM:
 			case CUBIC_BEZIER_PARAM:
@@ -704,19 +706,20 @@ void ofxAnimatable::drawCurve(int x, int y, int size, bool bg, ofColor c ){
 				pts.addColor(ofColor(ofColor::magenta,blink));
 				pts.addVertex(ofVec3f(xx + cubicBezierParamBx * size, yy + size - cubicBezierParamBy * size));
 				ofSetColor(ofColor::cyan);
-				ofDrawBitmapString("A", pts.getVertices()[0] + ofVec2f(5,4));
+
+				ofDrawBitmapString("A", pt0.x + 5, pt0.y + 4);
 				ofSetColor(ofColor::magenta);
-				ofDrawBitmapString("B", pts.getVertices()[1] + ofVec2f(5,4));
+				ofDrawBitmapString("B", pt1.x + 5, pt1.y + 4);
 				break;
 			case QUADRATIC_BEZIER_PARAM:
 				pts.addColor(ofColor(ofColor::blue,blink));
 				pts.addVertex(ofVec3f(xx + quadraticBezierParamAx * size, yy + size - quadraticBezierParamAy * size));
-				ofDrawBitmapString("QB", pts.getVertices()[0] + ofVec2f(5,4));
+				ofDrawBitmapString("QB", pt0.x + 5, pt0.y + 4);
 				break;
 			case EXPONENTIAL_SIGMOID_PARAM:
 				pts.addColor(ofColor(ofColor::cyan,blink));
 				pts.addVertex(ofVec3f(xx + doubleExpSigmoidParam * size, yy + size * 0.5));
-				ofDrawBitmapString("ES", pts.getVertices()[0] + ofVec2f(5,4));
+				ofDrawBitmapString("ES", pt0.x + 5, pt0.y + 4);
 				break;
 			case BOUNCE_IN_CUSTOM:
 			case BOUNCE_OUT_CUSTOM:
@@ -724,8 +727,8 @@ void ofxAnimatable::drawCurve(int x, int y, int size, bool bg, ofColor c ){
 				pts.addVertex(ofVec3f(xx + (bounceNumB / (BOUNCES_MAX-1)) * size, yy + size));
 				pts.addColor(ofColor(ofColor::purple,blink));
 				pts.addVertex(ofVec3f(xx , yy + size -bounceElast * size));
-				ofDrawBitmapString("B1", pts.getVertices()[0] + ofVec2f(5,4));
-				ofDrawBitmapString("BE", pts.getVertices()[1] + ofVec2f(5,4));
+				ofDrawBitmapString("B1", pt0.x + 5, pt0.y + 4);
+				ofDrawBitmapString("BE", pt1.x + 5, pt1.y + 4);
 				break;
 			default: break;
 		}
