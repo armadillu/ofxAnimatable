9d31001920c071dd9ab4f0dd71a15dacb1909402
Oriol Ferrer Mesia <oriol@uri.cat>
2017-08-08 16:37:29 -0400

    try fix appveyor ci

---
+++ scripts/ci/vs/install.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -e
+
+echo "Executing ci/vs/install.sh"
+
+export OF_ROOT="$APPVEYOR_BUILD_FOLDER/../openFrameworks"
+export OF_ADDONS=$OF_ROOT/addons
+
+pwd;
+echo "OF_ADDONS: $OF_ADDONS"
+
+ADDONS="armadillu/ofxTimeMeasurements armadillu/ofxRemoteUI armadillu/ofxHistoryPlot"
+
+
+cd $OF_ADDONS
+
+for ADDON in $ADDONS
+do
+  echo "Cloning addon '$ADDON' to " `pwd`
+  git clone --depth=1  https://github.com/$ADDON.git
+done
+
+cd -
